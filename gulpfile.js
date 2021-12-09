e2fecaee690bdc98981bb1d07a88e4cfa81d34e5
Khalid <59440312+alhajr20@users.noreply.github.com>
2021-12-09 23:00:00 +0300

    Завершает верстку

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,66 @@
-'use strict';
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const pug = require('gulp-pug');
+const babel = require('gulp-babel');
+const plumber = require('gulp-plumber');
 
-module.exports = require('bootstrap-gulp');
+gulp.task('server', function() {
+
+  browserSync({
+    server: {
+      baseDir: "dist"
+    }
+  });
+
+  gulp.watch("source/pug/*.pug").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+  return gulp.src("source/scss/**/*.+(scss|sass)")
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({suffix: '.min', prefix: ''}))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+  gulp.watch("source/scss/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+  gulp.watch("source/pug/*.pug").on('change', gulp.parallel('pug'));
+  gulp.watch("source/js/**/*.js").on('change', gulp.parallel('scripts'));
+  gulp.watch("source/img/**/*").on('all', gulp.parallel('images'));
+});
+
+gulp.task('pug', function () {
+  return gulp.src("source/pug/*.pug")
+    .pipe(plumber())
+    .pipe(pug({
+      pretty: false,
+    }))
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task('scripts', function () {
+  return gulp.src("source/js/**/*.js")
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest("dist/js"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('images', function () {
+  return gulp.src("source/img/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/img"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'pug', 'images'));
--- source/js/main.js
+++ source/js/main.js
@@ -1,23 +1,29 @@
-// Для добавления функционала используем классы с префиксом js, стилизовать по этим классам нельзя
+document.addEventListener('DOMContentLoaded', () => {
 
-$(document).ready(function(){
+  /* Скрипт для modal window */
+  const showBtn = document.querySelector('.btn-show'),
+        modalWindow = document.querySelector('.buy-window'),
+        closeBtn = document.querySelector('.buy-window .close');
 
-	// $('input[type=tel]')
-	// 	.inputmask("8 (999) 999 99 99");
+  showBtn.addEventListener('click', () => {
+    modalWindow.classList.add('bw-active');
+    document.style.overflow = 'hidden';
+  });
 
+  closeBtn.addEventListener('click', () => {
+    modalWindow.classList.remove('bw-active');
+    document.style.overflow = 'visible';
+  });
 
-	// $('.js-popup-img')
-	// 	.magnificPopup({
-	// 		type:'image',
-	// 		closeOnContentClick: true,
-	// 		fixedContentPos: true,
-	// 		mainClass: 'mfp-no-margins mfp-with-zoom',
-	// 		image: {
-	// 			verticalFit: true
-	// 		},
-	// 		zoom: {
-	// 			enabled: true,
-	// 			duration: 300
-	// 		}
-	// 	});
+  /* Скрипт для dropdown меню */
+  const burgerBtn = document.querySelector('.header__burger'),
+        navMenu = document.querySelector('.header__links');
+
+  console.log(burgerBtn);
+  console.log(navMenu);
+
+  burgerBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('header__links-active');
+    burgerBtn.classList.toggle('header__burger-active');
+  });
 });
